7a248b6d999ea2451bfeeb8ec6a79566ea2186b0
zred <zachary.redmond@gmail.com>
2014-07-20 14:51:14 -0500

    File paste and related functions have been added.

--- chunky.py
+++ chunky.py
@@ -1,17 +1,18 @@
-def chunker(input_file_name,chunksize=1048576):
-	with open(input_file_name, 'r+b') as source:
+import os,re,glob
+
+def chunker(ifn,cs=1048576):
+	with open(ifn, 'r+b') as src:
 		while True:
-			buff = source.read(chunksize)
+			buff = src.read(cs)
 			if not buff:
 				break
 			yield buff
 
-def unchunker(chunks, output_file_name):
-	with open(output_file_name, 'w+b') as target:
+def unchunker(chunks, ofn):
+	with open(ofn, 'w+b') as tgt:
 		for chunk in chunks:
-			target.write(chunk)
+			tgt.write(chunk)
 
-import os
 			
 def file_split(input_file_name, count):
 	input_file_name_size = os.lstat(input_file_name).st_size
@@ -21,9 +22,29 @@ def file_split(input_file_name, count):
 	read_list = [chunksize]*count
 	read_list[0] += chunksize_remainder
 	with open(input_file_name, 'r+b') as source:
+		read_index = 0
 		for read_size in read_list:
 			data = source.read(read_size)
-			with open(prefix + '.part' + str(read_list.index(read_size)), 'w+b') as target:
+			with open(prefix + '-' + ftype + '-part' + str(read_index), 'w+b') as target:
 				target.write(data)
+			read_index += 1
+			
+def tryint(s):
+	try: return int(s)
+	except: return s
 
-# identical read sizes in the read list cause an issue with target nameing.
+def alnumkey(s):
+	return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+	
+def hsort(l):
+	l.sort(key=alnumkey)
+
+def file_paste(prefix):
+	parts = glob.glob(prefix + '*' + '-part' + '*')
+	suffix = glob.glob(prefix + '*' + '-part' + '*')[0].split('-')[1]
+	hsort(parts)
+	for part in parts:
+		with open(part, 'r+b') as source:
+			data = source.read()
+			with open(prefix + '.' + suffix, 'a+b') as target:
+				target.write(data)
